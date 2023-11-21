eecca3877cc848242a08ca363df5abb3c424f5db
tirmidziahmad <tirmidziahmad05@gmail.com>
2023-11-21 15:08:13 +0700

    done

--- DeteksiKantuk.py
+++ DeteksiKantuk.py
@@ -1,21 +1,21 @@
 import cv2
-import tensorflow as tf
-from tensorflow.keras.models import load_model
+import tensorflow
+from tensorflow import keras
 import numpy as np
 import time
-#from pygame import mixer
+from pygame import mixer
 #import serial
-
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades 
                                      + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades
                                     + 'haarcascade_eye.xml')
-model = load_model(r'C:\Users\tirmi\Downloads\Drowsiness-Haar-Cascade\models\model.h5')
+model = keras.models.load_model(r'C:\Users\tirmi\Downloads\Drowsiness-Haar-Cascade\models\model.h5')
 
-#mixer.init()
-#sound= mixer.Sound(r'F:\Deep Learning\Drowsiness Detection\alarm.wav')
+mixer.init()
+sound= mixer.Sound(r'C:\Users\tirmi\Downloads\Drowsiness-Haar-Cascade\sound\alarm.wav')
 cap = cv2.VideoCapture(0)
 Score = 0
+Kedip = 0
 while True:
     ret, frame = cap.read()
     height,width = frame.shape[0:2]
@@ -44,36 +44,33 @@ while True:
         
         # if eyes are closed
         if prediction[0]<0.50:
-            cv2.putText(frame,'Mata Tertutup',(30,height-20),
+            Score = Score+1
+            cv2.putText(frame,'Tertutup',(30,height-20),
                         fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
                         fontScale=1,color=(255,255,255),
                        thickness=1,lineType=cv2.LINE_AA)
-            cv2.putText(frame,str(Score)+' seconds',(170,height-20),
+            cv2.putText(frame,str(Score)+' Detik',(170,height-20),
                         fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
                         fontScale=1,color=(255,255,255),
                        thickness=1,lineType=cv2.LINE_AA)
-            Score=Score+1
             if(Score>4):
                 cv2.putText(frame,'Mengantuk!',(30,height-90),
                             fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
                             fontScale=1,color=(0,0,255),
                        thickness=1,lineType=cv2.LINE_AA)
-                #s.write(b'a')
                 time.sleep(2)
-                #try:
-                    #sound.play()
-                #except:
-                    #pass
+                try:
+                    sound.play()
+                except:
+                    pass
                
             
         # if eyes are open
         elif prediction[0]>0.80:
-            cv2.putText(frame,'EMata Terbuka',(30,height-50),
+            cv2.putText(frame,'Mata Terbuka',(30,height-50),
                         fontFace=cv2.FONT_HERSHEY_COMPLEX_SMALL,
                         fontScale=1,color=(0,255,0),
                        thickness=1,lineType=cv2.LINE_AA)
-            
-            #s.write(b'b')
             time.sleep(2)
 
             Score = Score-1
